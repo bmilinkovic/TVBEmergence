e239d516a2ed3d5298ec3aeec810804817e287e7
borjan milinkovic <borjan.milinkovic@gmail.com>
2022-09-15 13:14:01 +0200

    "Updating oscillatory modelling"

--- test/py/subnet_sim_oscillator.py
+++ test/py/subnet_sim_oscillator.py
@@ -1,3 +1,6 @@
+import os
+
+import tvb.simulator.plot.plotter
 from tvb.simulator.lab import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,70 +9,149 @@ from networks.pynetworks.subset9Modular36 import subnet9mod36
 import time
 import scipy.io as sio
 
-### Saving figures setup ###
-### if we want to not save, just set save = False ###
+import seaborn as sns
+
+
+# Setting up saving options
+
 figureDir = "/Users/borjanmilinkovic/Documents/gitdir/TVBEmergence/results/figures/"
 savefig = True
-###########################
 
-### Saving results setup ###
-### if we want to not save, just set savemat = False ###
 dataDir = "/Users/borjanmilinkovic/Documents/gitdir/TVBEmergence/results/data/"
 resultsmat = True
-###########################
 
+# Starting analysis
 
 conn = subnet9mod36()
-monitors = simulator.monitors.SubSample(period=1.953125)
+monitors = simulator.monitors.SubSample(period=.9765625) # subsampling at 256Hz or every 3.90625 ms
 monitors.configure()
 
+
+# practice simulation for oscilatory dynamics
+
+simulation = simulator.Simulator(connectivity=connectivity.Connectivity.from_file(),
+                                  coupling=coupling.Linear(),
+                                  integrator=integrators.HeunStochastic(dt=0.5,
+                                                                        noise=noise.Additive(nsig=np.array([0.01]))),
+                                  model=models.Generic2dOscillator(a=np.array([2.2]), b=np.array([-1.0]),
+                                                                   c=np.array([0.0]),
+                                                                   d=np.array([0.1]), I=np.array([0.0]),
+                                                                   alpha=np.array([1.0]),
+                                                                   beta=np.array([0.2]), gamma=np.array([-1.0]),
+                                                                   e=np.array([0.0]),
+                                                                   g=np.array([1.0]), f=np.array([0.333]),
+                                                                   tau=np.array([1.25])),
+                                  monitors=[monitors],
+                                  simulation_length=1000
+                                  )
+simulation.configure()
+
+test_results = simulation.run()
+
+test_time = test_results[0][0].squeeze()
+test_data = test_results[0][1].squeeze()
+
+
+
 # Set up the simulation with all the correct arguments.
-simulation = simulator.Simulator(connectivity=conn,
+simulation = simulator.Simulator(connectivity=connectivity.Connectivity.from_file(),
                                  coupling=coupling.Linear(),
-                                 integrator=integrators.HeunStochastic(dt=0.05, noise=noise.Additive(nsig = np.array([0.004]))),
-                                 model=models.Generic2dOscillator(),
+                                 integrator=integrators.HeunStochastic(dt=0.5, noise=noise.Additive(nsig = np.array([0.01]))),
+                                 model=models.Generic2dOscillator(a=np.array([2.2]), b=np.array([-1.0]), c=np.array([0.0]),
+                                                                  d=np.array([0.1]), I=np.array([0.0]), alpha=np.array([1.0]),
+                                                                  beta=np.array([0.2]), gamma=np.array([-1.0]), e=np.array([0.0]),
+                                                                  g=np.array([1.0]), f=np.array([0.333]), tau=np.array([1.25])),
                                  monitors=[monitors],
-                                 simulation_length=2000)
+                                 simulation_length=1000)
 simulation.configure()
 
 
 # Defining a function to run a simulation
 def run_sim(global_coupling):
     simulation.coupling.a = global_coupling
-    print("Starting Gen2dOscillator simulation with coupling factor " + str(global_coupling))
+    print("Starting Generic2dOscillator simulation with coupling factor " + str(global_coupling))
     results = simulation.run()
     data = results[0][1].squeeze()
     return (global_coupling, data)
 
 # running a parameter sweep of the global coupling parameter
-gc_range = np.arange(0.0, 5.1, .50)
+gc_range = np.arange(0.0, 3.1, .20)
 data = []
 for gc in gc_range:
     data.append((run_sim(np.array([gc]))))
 
-#plot connectivity
+# computing correlation coefficient
 
-plt.figure()
-utils.pyutils.connMatrixPlotter.connMatrixPlotter(conn)
-f1 = plt.gcf()
+def compute_corr(time_line, data_result, sim):
+    input_shape = data_result.shape
+    result_shape = (input_shape[1], input_shape[1])
+    sample_period = sim.monitors[0].period
+    t_start = sample_period
+    t_end = time_line[-1]
+    t_lo = int((1. / sample_period) * (t_start - sample_period))
+    t_hi = int((1. / sample_period) * (t_end - sample_period))
+    t_lo = max(t_lo,0)
+    t_hi = max(t_hi, input_shape[0])
+    FC = np.zeros(result_shape)
+    FC[:,:] = np.corrcoef(data_result.T)
+    return FC
+
+FC = compute_corr(test_time, test_data, simulation)
+#plot functional connectivity
+mask = np.triu(np.ones_like(FC, dtype=bool))
+f, ax = plt.subplots(figsize=(11,9))
+cmap = sns.diverging_palette(230,20, as_cmap=True)
+sns.heatmap(FC, mask=mask, cmap=cmap, vmax=FC.max(), vmin=FC.min(), center=0, square=True, linewidths=.5,
+            cbar_kws={"shrink":.5}, xticklabels=simulation.connectivity.region_labels, yticklabels=simulation.connectivity.region_labels)
 plt.show()
 
-# This below needs some work.
+sns.clustermap(FC, cmap=cmap, )
+plt.show()
+
+sns.clustermap(FC, cmap=cmap, metric="correlation")
+plt.show()
 
 
-# SAVING DATA FOR MVGC USE
+# plt.figure()
+# utils.pyutils.connMatrixPlotter.connMatrixPlotter(conn)
+# f1 = plt.gcf()
+# plt.show()
 
 
-if resultsmat:
-for i in range(len(data)):
-    if os.path.exists(dataDir + 'oscSim.mat'):
-        sio.savemat(dataDir + 'oscSim_{0}_{1}.mat'.format(i, int(time.time())), {'data': data[i][1]})
+# UNDER CONSTRUCTION - PLOTTING TIMESERIES PROPERLY
+# MATPLOTLIB example that doesn't currently work.
+
+# nodes, time = data[0][1]
+# segs= []
+# for i in range(nodes):
+#     segs.append(np.hstack((data[0][1][:,i]))
+#
+# offsets = np.zeros((nodes, 2), dtype=float)
+#
+# line_segments = matplotlib.collections.LineCollections(segs, offsets=offsets)
 
 
+
+
+
+
+# plotting time-series of oscillatory activity and saving
 
 if savefig:
-    if os.path.exists(figureDir + 'oscFig.png'):
-        plt.savefig(figureDir + 'oscFig_{}.png'.format(int(time.time())))
-    else:
-        plt.savefig(figureDir + 'oscFig.png')
+    fileNameTemplate = r'/Users/borjanmilinkovic/Documents/gitdir/TVBEmergence/results/figures/oscPlot_{0:02d}.png'
+
+    for i in range(len(data)):
+        plt.figure()
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Oscillatory activity')
+        plt.plot(data[i][1])
+        plt.savefig(fileNameTemplate.format(i), format='png')
+        plt.clf()
+
+# SAVING DATA FOR MVGC USE
+if resultsmat:
+    for i in range(len(data)):
+        if os.path.exists(dataDir + 'oscSim.mat'):
+            sio.savemat(dataDir + 'oscSim_{0}_{1}.mat'.format(i, int(time.time())), {'data': data[i][1]})
+
 
