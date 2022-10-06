1d08b787937ba1b8f1915bbcc74e9c295c0682bc
borjan milinkovic <borjan.milinkovic@gmail.com>
2022-10-06 20:39:31 +0200

    built pipeline for ssdi on subnetworks of osc2d

---
+++ dd_tvbsims.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = dd_tvbsims(inputArg1,inputArg2)
+%DD_TVBSIMS Summary of this function goes here
+%   Detailed explanation goes here
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
