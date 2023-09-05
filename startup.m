47e598b61daaac7014858838d045a025840fc80e
Borjan Milinkovic <borjan.milinkovic@gmail.com>
2023-09-05 13:51:01 +1000

    updating startup

--- startup.m
+++ startup.m
@@ -4,14 +4,14 @@ addpath(emergenceDir);
 
 addpath(fullfile(emergenceDir, 'deprecated'));
 addpath(genpath(fullfile(emergenceDir, 'networks')));
-addpath(genpath(fullfile(emergenceDir, 'results')));
+addpath(genpath(fullfile(emergenceDir, 'plots')));
 addpath(fullfile(emergenceDir, 'simulation'));
 addpath(fullfile(emergenceDir, 'utils'));
 addpath(fullfile(emergenceDir, 'test'));
 addpath(fullfile(emergenceDir, 'test/matlab'));
+addpath(fullfile(emergenceDir, 'utils'));
 
-
-ssdi_path = getenv('SSDIDIR');
+ssdi_path = getenv('ssdi');
 assert(exist(ssdi_path,'dir') == 7,'bad SSDI path: ''%s'' does not exist or is not a directory',ssdi_path);
 run(fullfile(ssdi_path,'startup'));
 fprintf('[Emergence Pipeline startup] Added path to State-Space Dynamical Indpendence toolbox: %s\n',ssdi_path);
